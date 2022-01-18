e13ad91ee7f13eedd846fe80ada85e7e040b1522
matheustkaczyk <matheus111904@hotmail.com>
2022-01-18 12:35:14 -0300

    Estruturacao de pastas, servidor express com basico listener de socket montado

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-// Faça seu código aqui
+const express = require('express');
+const socketIo = require('socket.io');
+const path = require('path');
+const cors = require('cors');
+const http = require('http');
+
+const PORT = 3000 || process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const httpServer = http.createServer(app);
+
+const io = socketIo(httpServer, {
+  cors: {
+    origin: 'https://localhost:3000',
+    method: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`Alguem se conectou ${socket.id}`);
+});
+
+app.get('/', (_req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, './src/views/index.html'));
+});
+
+httpServer.listen(PORT, () => console.log(`Conectado na porta ${PORT}`));
